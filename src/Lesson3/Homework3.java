1a65c0c6b80eb8ccd06c992a3b7cf1005cc62673
macbookpro <Lis.ignat@mail.ru>
2022-06-23 01:00:39 +0300

    OOP CreditCard1

--- src/Lesson3/Homework3.java
+++ src/Lesson3/Homework3.java
@@ -7,18 +7,16 @@ public class Homework3 {
 
     public static void main(String[] args) {
 
-        int[] arrys = new int[4];
-        arrys[0] = (2);
-        arrys[1] = (3);
-        arrys[2] = (4);
-        arrys[3] = (5);
-
-        arrys[1] = 0;
+        int[] arrys = {2,3,4,5};
+        int nuberToDelete = 3;
 
 
-        for (int i = 2; i < arrys.length - 1; i++) {
-            arrys[i - 1] = arrys[i];
-            arrys[i] = 0;
+
+        for (int i = 0; i < arrys.length; i++) {
+            if (arrys[i]==nuberToDelete) {
+                arrys[i] = 0;
+
+            }
 
         }
         System.out.println(Arrays.toString(arrys));
---
+++ src/Lesson6/Operation.java
@@ -0,0 +1,22 @@
+package Lesson6;
+
+public class Operation {
+    public void deposite(CreditCard creditCard, int depositeMoney) {
+        int result = creditCard.getSum() + depositeMoney;
+        creditCard.setSum(result);
+    }
+
+
+    public void huiCard(CreditCard creditCard, int vikeNaSushi) {
+        int zpIgnata = creditCard.getSum() - vikeNaSushi;
+        creditCard.setSum(zpIgnata);
+    }
+    public void minus(CreditCard creditCard, int minusMoney) {
+        int result = creditCard.getSum() - minusMoney;
+        creditCard.setSum(result);
+    }
+
+    public void showInfo(CreditCard creditCard) {
+        System.out.println("Информация о карте: номер карты " + creditCard.getNumber() + " Сумма карты " + creditCard.getSum());
+    }
+}
