b180e6274eb91d5f85ba20459e92491217058326
macbookpro <Lis.ignat@mail.ru>
2022-09-24 16:45:19 +0300

    Task

--- src/Task2/Tree.java
+++ src/Task2/Tree.java
@@ -1,8 +1,5 @@
 package Task2;
-import javax.swing.tree.TreeNode;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+
 
 public class Tree {
     public static void main(String[] args) {
--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -1,6 +1,6 @@
 package Task3;
 
-import Task3.MyArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
